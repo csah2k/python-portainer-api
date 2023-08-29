8fe2f377c52bdf7193466d060e0707d0ae048127
Cassio Schulte Agnelli <cassio.schulte@imexperts.com.br>
2023-08-29 00:50:43 +0000

    stop/start/restart container

--- portainer_api/api/endpoints_api.py
+++ portainer_api/api/endpoints_api.py
@@ -380,7 +380,7 @@ class EndpointsApi(object):
         Retrieve details abount an endpoint. **Access policy**: restricted
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
-        >>> thread = api.endpoint_inspect(id, async_req=True)
+        >>> thread = api.endpoint_docker_container_status(id, async_req=True)
         >>> result = thread.get()
 
         :param async_req bool
@@ -400,7 +400,7 @@ class EndpointsApi(object):
         Retrieve details abount an endpoint. **Access policy**: restricted
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
-        >>> thread = api.endpoint_inspect_with_http_info(id, async_req=True)
+        >>> thread = api.endpoint_docker_container_status_with_http_info(id, async_req=True)
         >>> result = thread.get()
 
         :param async_req bool
@@ -419,18 +419,18 @@ class EndpointsApi(object):
             if key not in all_params:
                 raise TypeError(
                     "Got an unexpected keyword argument '%s'"
-                    " to method endpoint_inspect" % key
+                    " to method endpoint_docker_container_status" % key
                 )
             params[key] = val
         del params['kwargs']
         # verify the required parameter 'id' is set
         if ('id' not in params or
                 params['id'] is None):
-            raise ValueError("Missing the required parameter `id` when calling `endpoint_inspect`")
+            raise ValueError("Missing the required parameter `id` when calling `endpoint_docker_container_status`")
         # verify the required parameter 'cid' is set
         if ('cid' not in params or
                 params['cid'] is None):
-            raise ValueError("Missing the required parameter `cid` when calling `endpoint_inspect`")
+            raise ValueError("Missing the required parameter `cid` when calling `endpoint_docker_container_status`")
         
         collection_formats = {}
 
@@ -465,6 +465,288 @@ class EndpointsApi(object):
             post_params=form_params,
             files=local_var_files,
             response_type='object',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+    
+    def endpoint_docker_container_stop(self, id, cid, **kwargs):
+        """Stop docker container
+
+        Retrieve details abount an endpoint. **Access policy**: restricted
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.endpoint_docker_container_stop(id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param int id: Endpoint identifier (required)
+        :param int id: Container identifier (required)
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.endpoint_docker_container_stop_with_http_info(id, cid, **kwargs)
+        else:
+            (data) = self.endpoint_docker_container_stop_with_http_info(id, cid, **kwargs)
+            return data
+
+    def endpoint_docker_container_stop_with_http_info(self, id, cid, **kwargs):
+        """Stop docker container
+
+        Retrieve details abount an endpoint. **Access policy**: restricted
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.endpoint_docker_container_stop_with_http_info(id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param int id: Endpoint identifier (required)
+        param int id: Container identifier (required)
+        """
+
+        all_params = ['id', 'cid']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method endpoint_docker_container_stop" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'id' is set
+        if ('id' not in params or
+                params['id'] is None):
+            raise ValueError("Missing the required parameter `id` when calling `endpoint_docker_container_stop`")
+        # verify the required parameter 'cid' is set
+        if ('cid' not in params or
+                params['cid'] is None):
+            raise ValueError("Missing the required parameter `cid` when calling `endpoint_docker_container_stop`")
+        
+        collection_formats = {}
+
+        path_params = {}
+        if 'id' in params:
+            path_params['id'] = params['id']
+        if 'cid' in params:
+            path_params['cid'] = params['cid']
+
+        query_params = {}
+        header_params = {}        
+        form_params = []
+        local_var_files = {}
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['jwt']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/endpoints/{id}/docker/containers/{cid}/stop', 'POST',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='str',
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+    
+    def endpoint_docker_container_start(self, id, cid, **kwargs):
+        """Start docker container
+
+        Retrieve details abount an endpoint. **Access policy**: restricted
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.endpoint_docker_container_start(id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param int id: Endpoint identifier (required)
+        :param int id: Container identifier (required)
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.endpoint_docker_container_start_with_http_info(id, cid, **kwargs)
+        else:
+            (data) = self.endpoint_docker_container_start_with_http_info(id, cid, **kwargs)
+            return data
+
+    def endpoint_docker_container_start_with_http_info(self, id, cid, **kwargs):
+        """Start docker container
+
+        Retrieve details abount an endpoint. **Access policy**: restricted
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.endpoint_docker_container_start_with_http_info(id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param int id: Endpoint identifier (required)
+        param int id: Container identifier (required)
+        """
+
+        all_params = ['id', 'cid']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method endpoint_docker_container_start" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'id' is set
+        if ('id' not in params or
+                params['id'] is None):
+            raise ValueError("Missing the required parameter `id` when calling `endpoint_docker_container_start`")
+        # verify the required parameter 'cid' is set
+        if ('cid' not in params or
+                params['cid'] is None):
+            raise ValueError("Missing the required parameter `cid` when calling `endpoint_docker_container_start`")
+        
+        collection_formats = {}
+
+        path_params = {}
+        if 'id' in params:
+            path_params['id'] = params['id']
+        if 'cid' in params:
+            path_params['cid'] = params['cid']
+
+        query_params = {}
+        header_params = {}        
+        form_params = []
+        local_var_files = {}
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['jwt']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/endpoints/{id}/docker/containers/{cid}/start', 'POST',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='str',
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+    
+    def endpoint_docker_container_restart(self, id, cid, **kwargs):
+        """Restart docker container
+
+        Retrieve details abount an endpoint. **Access policy**: restricted
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.endpoint_docker_container_restart(id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param int id: Endpoint identifier (required)
+        :param int id: Container identifier (required)
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.endpoint_docker_container_restart_with_http_info(id, cid, **kwargs)
+        else:
+            (data) = self.endpoint_docker_container_restart_with_http_info(id, cid, **kwargs)
+            return data
+
+    def endpoint_docker_container_restart_with_http_info(self, id, cid, **kwargs):
+        """Restart docker container
+
+        Retrieve details abount an endpoint. **Access policy**: restricted
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.endpoint_docker_container_restart_with_http_info(id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param int id: Endpoint identifier (required)
+        param int id: Container identifier (required)
+        """
+
+        all_params = ['id', 'cid']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method endpoint_docker_container_restart" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'id' is set
+        if ('id' not in params or
+                params['id'] is None):
+            raise ValueError("Missing the required parameter `id` when calling `endpoint_docker_container_restart`")
+        # verify the required parameter 'cid' is set
+        if ('cid' not in params or
+                params['cid'] is None):
+            raise ValueError("Missing the required parameter `cid` when calling `endpoint_docker_container_restart`")
+        
+        collection_formats = {}
+
+        path_params = {}
+        if 'id' in params:
+            path_params['id'] = params['id']
+        if 'cid' in params:
+            path_params['cid'] = params['cid']
+
+        query_params = {}
+        header_params = {}        
+        form_params = []
+        local_var_files = {}
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['jwt']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/endpoints/{id}/docker/containers/{cid}/restart', 'POST',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='str',
             auth_settings=auth_settings,
             async_req=params.get('async_req'),
             _return_http_data_only=params.get('_return_http_data_only'),
